17438fee44e3e321ef784d895deccad6b86655dd
Jaeseung Lee(Home) <41176085+tkxkd0159@users.noreply.github.com>
2021-07-12 07:41:12 +0900

    Add some functions Add sign function for certification Modify contract API function Modify backend function

--- app.js
+++ app.js
@@ -1,35 +1,65 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
+import Web3 from 'web3';
 
-import {makeErcMeta} from './lib/utils.mjs';
+import { makeErcMeta } from './lib/utils.mjs';
+import { mintAsset } from './lib/energy.mjs';
 
 const __dirname = path.resolve();
-const app = express();
-const port = 3000;
-const token_path = '../ERC1155meta/token/';
 
-app.use('/static', express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
-app.use(express.json());
+const main_addr = process.env.GAN_ADDR1;
+const sub_addr = process.env.GAN_ADDR2;
+const asset_addr = process.env.ASSET_ADDR;
+const trade_addr = process.env.TRADE_ADDR;
+const eth_to_wei = Math.pow(10, 18)
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+const web3 = new Web3("http://127.0.0.1:7545");
+const asset_abi = JSON.parse(fs.readFileSync('./energy/build/contracts/EnergyAsset.json')).abi;
+const asset_contract = new web3.eth.Contract(asset_abi, asset_addr);
+const trade_abi = JSON.parse(fs.readFileSync('./energy/build/contracts/EnergyTrade.json')).abi;
+const trade_contract = new web3.eth.Contract(trade_abi, trade_addr);
 
+function initApiServer() {
+    const app = express();
+    const port = 3000;
+    const token_path = '../ERC1155meta/token/';
 
-
-app.route('/energy')
-  .get(function (req, res) {
-    let meta_obj = makeErcMeta(token_path, "LJS", 60, 1000, 20);
-    res.send(meta_obj)
-  })
-  .post(function (req, res) {
-    let temp  = req.body['hello'];
-    console.log(temp)
-    res.send(req.body);
-  })
+    app.use('/static', express.static(path.join(__dirname, 'public')));
+    app.set('view engine', 'ejs');
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
 
-  app.listen(port, () => {
-    console.log(`My app listening at http://localhost:${port}`)
-  })
+    app.get('/', (req, res) => {
+        res.render('base', {type: "tx"});
+    });
+
+
+    app.route('/register')
+      .get(function (req, res) {
+        res.render('base', {type: "register"})
+      })
+      .post(function (req, res) {
+        let data  = req.body;
+
+        (async function () {
+          let [meta_obj, file_nums] = await makeErcMeta(token_path, data["from_"],
+          data["start_"], data["Period"],
+          data["Power"], data['Price']);
+          mintAsset(data["from_"], file_nums, 1, data["from_"], asset_contract);
+          console.log(meta_obj);
+          console.log(file_nums);
+          res.redirect('/register');
+        })();
+      })
+
+
+      app.listen(port, () => {
+        console.log(`My app listening at http://localhost:${port}`)
+      })
+}
+
+(function () {
+    initApiServer();
+})();
--- test.js
+++ test.js
@@ -1,23 +1,39 @@
+import fs from 'fs';
+import Web3 from 'web3';
+
 import {main_addr,
     getUriMeta, getAssetContractOwner, getBalanceBatch, getEtokenTotalBalance,
     setURI, batchTransferFrom, mintAsset, mintBatchAsset, burnBatchAsset
 } from "./lib/energy.mjs";
 
-const sub_addr = process.env.GAN_ADDR2;
 
 // setURI("https://tkxkd0159.github.io/ERC1155meta/token/{id}.json", main_addr);
 
-const TEST_TYPE = "set"
+const sub_addr = process.env.GAN_ADDR2;
+const asset_addr = process.env.ASSET_ADDR;
+const trade_addr = process.env.TRADE_ADDR;
+const eth_to_wei = Math.pow(10, 18)
+
+const web3 = new Web3("http://127.0.0.1:7545");
+const asset_abi = JSON.parse(fs.readFileSync('./energy/build/contracts/EnergyAsset.json')).abi;
+const asset_contract = new web3.eth.Contract(asset_abi, asset_addr);
+const trade_abi = JSON.parse(fs.readFileSync('./energy/build/contracts/EnergyTrade.json')).abi;
+const trade_contract = new web3.eth.Contract(trade_abi, trade_addr);
+
+
+const TEST_TYPE = "get"
 
 if (TEST_TYPE === "get") {
-    const myuri = await getUriMeta(1);
+
+    const myuri = await getUriMeta(1, asset_contract);
     console.log(myuri);
-    let balances = await getBalanceBatch([main_addr, main_addr],[0, 1], main_addr);
+    let balances = await getBalanceBatch([main_addr, main_addr, main_addr, main_addr, main_addr, main_addr, main_addr, main_addr, main_addr, main_addr],
+                                        [0, 1, 2, 3, 4, 5, 6, 7, 8, 12], main_addr, asset_contract);
     console.log(balances);
-    const asset_owner = await getAssetContractOwner(main_addr);
-    console.log(asset_owner);
-    const total_etoken_balance = await getEtokenTotalBalance(main_addr);
-    console.log(total_etoken_balance);
+    // const asset_owner = await getAssetContractOwner(main_addr);
+    // console.log(asset_owner);
+    // const total_etoken_balance = await getEtokenTotalBalance(main_addr);
+    // console.log(total_etoken_balance);
 } else if (TEST_TYPE === "set") {
     let balances = await getBalanceBatch([main_addr, main_addr, sub_addr, sub_addr],[0, 1, 0, 1], main_addr);
     console.log(balances);
