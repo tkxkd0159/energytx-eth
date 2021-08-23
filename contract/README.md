5c8150b79b9bd1d25431761998d046c377512cae
ljs_ku_linux <41176085+tkxkd0159@users.noreply.github.com>
2021-08-23 09:17:52 +0900

    Make scripts

--- contract/README.md
+++ contract/README.md
@@ -15,3 +15,9 @@ truffle init
 truffle compile
 truffle migrate [--reset] --network goerli  # compile + deploy
 ```
+
+# ERC1155
+
+mint되는 각 토큰은 고유하고 수량을 정할 수 있음. 토큰의 정보는 외부 메타데이터를 통해 받아옴.  
+The `uri` can include the string `{id}` which clients must replace with the actual token ID, in lowercase hexadecimal (with no 0x prefix) and leading zero padded to **64 hex** characters.  
+16^64개의 NFT metadata 생성 가능
---
+++ script/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+options=$(getopt -o '' --long ipfs -- "$@")
+
+cwd=$(pwd)
+eval set -- "$options"
+while true; do
+    case "$1" in
+        --ipfs)
+        echo " * Start to install IPFS"
+        wget https://dist.ipfs.io/go-ipfs/v0.9.0/go-ipfs_v0.9.0_linux-amd64.tar.gz
+        tar -xvzf go-ipfs_v0.9.0_linux-amd64.tar.gz
+        cd go-ipfs
+        sudo bash install.sh
+        cd $cwd
+        ipfs init
+        echo " * Finish IPFS installation"
+            shift ;;
+        --)
+            shift
+            break ;;
+    esac
+done
+
+echo " * Start to install background packages "
+cd ./server && npm i
+echo " * Finish installation 1/2 "
+echo " * Start to install frontend packages "
+cd ../client && npm i
+npm run build
+echo " * Finish installation 2/2 "
